ea4506c91058c9f1edc7da8ac0f39a146b602175
Matt Rycraft <mattrycraft@comcast.net>
2018-02-12 21:55:45 -0600

    Update MileageRedemptionApp.java

--- Assignment1/MileageRedemptionApp.java
+++ Assignment1/MileageRedemptionApp.java
@@ -6,13 +6,14 @@ Assingment 1
 */
 
 import java.io.File;
+import java.io.IOException;
 import java.util.Scanner;
+
 
 public class MileageRedemptionApp {
 
-    public static void main(String[] args){
-        char choice;
-
+    public static void main(String[] args) {
+        MilesRedeemer m = new MilesRedeemer();
         Scanner reader = new Scanner(System.in);
         System.out.println("Please input your total accumulated miles: ");
         int mileage = reader.nextInt();
@@ -21,10 +22,31 @@ public class MileageRedemptionApp {
         int month = reader.nextInt();
 
         System.out.println("Do you wish to Continue(y/n): ");
-        choice = reader.next().charAt(0);
+        char choice = reader.next().charAt(0);
 
         reader.close();
 
+
+        while (choice == 'y')
+        {
+
+            File file = new File(args[0]);
+
+            Scanner scan = new Scanner(System.in);
+
+
+            try {
+                Scanner input = new Scanner(file);
+                m.readDestinations(input);
+            } catch (IOException e) {
+                System.out.println(e.getMessage());
+            }
+
+
+        }
     }
+
+
 }
+
 

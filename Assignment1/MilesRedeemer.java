bf977f9e402de4ad4f78ea64736be665cbd2d693
Matt Rycraft <mattrycraft@comcast.net>
2018-02-12 21:54:54 -0600

    Add files via upload

---
+++ Assignment1/MilesRedeemer.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class MilesRedeemer {
+
+    public void readDestinations(Scanner scanFile) {
+        StringTokenizer line;
+
+        while (scanFile.hasNextLine()) {
+            
+
+
+
+        }
+    }
+
+}
+

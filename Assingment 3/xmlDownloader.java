b411f5ea19c2e12a1db167cfe91e0c5f1ce8bf51
Matt Rycraft <mattrycraft@comcast.net>
2018-04-02 18:25:08 -0500

    Update xmlDownloader.java

--- Assingment 3/xmlDownloader.java
+++ Assingment 3/xmlDownloader.java
@@ -24,6 +24,15 @@ public class xmlDownloader extends JFrame {
         Type = new JMenu("Type");
         Limit = new JMenu("Limit");
         Explicit = new JMenu("Explicit");
+        newMusicOption = new JMenuItem("New Music");
+        recentMusicOption = new JMenuItem("Recent Music");
+        topAlbumsOption = new JMenuItem("Top Albums");
+        tenOption = new JMenuItem("10");
+        twentyFiveOption = new JMenuItem("25");
+        fiftyOption = new JMenuItem("50");
+        oneHundOption = new JMenuItem("100");
+        yesOption = new JMenuItem("Yes");
+        noOption = new JMenuItem("No");
         setLayout(new BorderLayout());
 
 
@@ -37,6 +46,15 @@ public class xmlDownloader extends JFrame {
         menuBar.add(Type);
         menuBar.add(Limit);
         menuBar.add(Explicit);
+        Type.add(newMusicOption);
+        Type.add(recentMusicOption);
+        Type.add(topAlbumsOption);
+        Limit.add(tenOption);
+        Limit.add(twentyFiveOption);
+        Limit.add(fiftyOption);
+        Limit.add(oneHundOption);
+        Explicit.add(yesOption);
+        Explicit.add(noOption);
         add(menuBar, BorderLayout.NORTH);
         xmlPanel.add(getAlbums,BorderLayout.CENTER);
         xmlPanel.add(albumPane,BorderLayout.SOUTH);
@@ -71,5 +89,14 @@ public class xmlDownloader extends JFrame {
     private JMenu Type;
     private JMenu Limit;
     private JMenu Explicit;
+    private JMenuItem newMusicOption;
+    private JMenuItem recentMusicOption;
+    private JMenuItem topAlbumsOption;
+    private JMenuItem tenOption;
+    private JMenuItem twentyFiveOption;
+    private JMenuItem fiftyOption;
+    private JMenuItem oneHundOption;
+    private JMenuItem yesOption;
+    private JMenuItem noOption;
 
 }

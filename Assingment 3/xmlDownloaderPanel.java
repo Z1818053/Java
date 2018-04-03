63daf3c9ea513fd80f66a7eabac9c0a9c70330fa
Matt Rycraft <mattrycraft@comcast.net>
2018-04-03 08:14:23 -0500

    Add files via upload
    This is the button and jtextarea we need, adding it to xmlDownloader, however having issues correctly displaying everything

---
+++ Assingment 3/xmlDownloaderPanel.java
@@ -0,0 +1,39 @@
+import javax.swing.*;
+import java.awt.*;
+
+
+public class xmlDownloaderPanel extends JPanel {
+
+    public xmlDownloaderPanel(){
+        createXMLPanel();
+    }
+
+    //Creates and displays the gui
+    public void createXMLPanel() {
+
+        //initialize variables
+        xmlPanel = new JPanel();
+        getAlbums = new JButton("Get Albums");
+        showAlbums = new JTextArea();
+        albumPane = new JScrollPane(showAlbums);
+        xmlPanel.setLayout(new BorderLayout());
+
+        //set size of window
+        xmlPanel.setPreferredSize(new Dimension(800,500));
+        albumPane.setPreferredSize(new Dimension(800,500));
+
+
+        //add button and jtextarea
+        xmlPanel.add(getAlbums,BorderLayout.CENTER);
+        xmlPanel.add(albumPane,BorderLayout.SOUTH);
+
+
+    }
+
+
+    //Private members
+    private JPanel xmlPanel;
+    private JButton getAlbums;
+    private JTextArea showAlbums;
+    private JScrollPane albumPane;
+}

5b009c45df9e50b8121e1ea2dd6b73f01e008823
Matt Rycraft <mattrycraft@comcast.net>
2018-04-04 10:02:35 -0500

    Update xmlDownloader.java

--- Assingment 3/xmlDownloader.java
+++ Assingment 3/xmlDownloader.java
@@ -1,84 +1,158 @@
+/***************************
+ * Matt Rycraft - z1818053
+ * Tommy Franczak - z1796882
+ *
+ * CSCI 470
+ *
+ * Assignment 3
+ *
+ * This program will download data from a url, parse it and display it
+ */
+
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 
-public class xmlDownloader extends JFrame {
+public class xmlDownloader extends JFrame implements ActionListener {
 
+    // Constructor for the JFrame
+    private xmlDownloader(String title) {
+        // Set the title
+        super(title);
+    }
 
-    public xmlDownloader(){
-        createAndShowGUI();
-        setTitle("iTunes Store Albums");
-        setSize(new Dimension(800,500));
+    //adds borderlayout to the frame and sets it to visible
+    private void createAndShowGUI() {
+        setLayout( new BorderLayout() );
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        createMenu();
+        add(xmlPanel, BorderLayout.CENTER);
+        pack();
         setVisible(true);
     }
 
-    //Creates and displays the gui
-    public void createAndShowGUI() {
+    //Creates the menu and its items with functionality
+    private void createMenu() {
+        // Create type menu
+        menu = new JMenu("Type");
+        group = new ButtonGroup();
 
-        //initialize variables
-        menuBar = new JMenuBar();
-        showAlbums = new JTextArea();
-        Type = new JMenu("Type");
-        Limit = new JMenu("Limit");
-        Explicit = new JMenu("Explicit");
-        newMusicOption = new JMenuItem("New Music");
-        recentMusicOption = new JMenuItem("Recent Music");
-        topAlbumsOption = new JMenuItem("Top Albums");
-        tenOption = new JMenuItem("10");
-        twentyFiveOption = new JMenuItem("25");
-        fiftyOption = new JMenuItem("50");
-        oneHundOption = new JMenuItem("100");
-        yesOption = new JMenuItem("Yes");
-        noOption = new JMenuItem("No");
-        setLayout(new BorderLayout());
+        // Create new music, recent releases and top albums entry along with actionListeners
+        menuItem = new JRadioButtonMenuItem("New Music");
+        menuItem.setSelected(true);
+        menuItem.addActionListener(this);
+        group.add(menuItem);
+        menu.add(menuItem);
+        menuItem = new JRadioButtonMenuItem("Recent Releases");
+        menuItem.addActionListener(this);
+        group.add(menuItem);
+        menu.add(menuItem);
+        menuItem = new JRadioButtonMenuItem("Top Albums");
+        menuItem.addActionListener(this);
+        group.add(menuItem);
+        menu.add(menuItem);
 
-        //add menubar to frame
-        menuBar.add(Type);
-        menuBar.add(Limit);
-        menuBar.add(Explicit);
-        Type.add(newMusicOption);
-        Type.add(recentMusicOption);
-        Type.add(topAlbumsOption);
-        Limit.add(tenOption);
-        Limit.add(twentyFiveOption);
-        Limit.add(fiftyOption);
-        Limit.add(oneHundOption);
-        Explicit.add(yesOption);
-        Explicit.add(noOption);
-        add(menuBar, BorderLayout.NORTH);
+        //add type to menu
+        menuBar.add(menu);
 
-        add(new xmlDownloaderPanel());
+        // Create limit menu
+        menu = new JMenu("Limit");
+        group = new ButtonGroup();
 
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        pack();
+        // Create 10, 25, 50, and 100 entry along with actionlistener
+        menuItem = new JRadioButtonMenuItem("10");
+        menuItem.setSelected(true);
+        menuItem.addActionListener(this);
+        group.add(menuItem);
+        menu.add(menuItem);
+        menuItem = new JRadioButtonMenuItem("25");
+        menuItem.addActionListener(this);
+        group.add(menuItem);
+        menu.add(menuItem);
+        menuItem = new JRadioButtonMenuItem("50");
+        menuItem.addActionListener(this);
+        group.add(menuItem);
+        menu.add(menuItem);
+        menuItem = new JRadioButtonMenuItem("100");
+        menuItem.addActionListener(this);
+        group.add(menuItem);
+        menu.add(menuItem);
 
+        //add limit to menu
+        menuBar.add(menu);
+
+        // Create explicit menu
+        menu = new JMenu("Explicit");
+        group = new ButtonGroup();
+
+        // Create yes and no entry along with action listener
+        menuItem = new JRadioButtonMenuItem("Yes");
+        menuItem.addActionListener(this);
+        menuItem.setSelected(true);
+        group.add(menuItem);
+        menu.add(menuItem);
+        menuItem = new JRadioButtonMenuItem("No");
+        menuItem.addActionListener(this);
+        menuItem.setSelected(true);
+        group.add(menuItem);
+        menu.add(menuItem);
+
+        //add explicit to menu
+        menuBar.add(menu);
+
+        // sets the JMenuBar to the menuBar.
+        setJMenuBar(menuBar);
     }
 
-    public static void main(String[] args){
-        EventQueue.invokeLater(new Runnable() {
-            @Override
-            public void run() {
-                new xmlDownloader();
-            }
+    //This function will find out what the menu items are selected as and set the variables accordingly for use in
+    // the url
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        switch (e.getActionCommand())
+        {
+            case "New Music":
+                xmlPanel.setType("new-music");
+                break;
+
+            case "Recent Releases":
+                xmlPanel.setType("recent-releases");
+                break;
+
+            case "Top Albums":
+                xmlPanel.setType("top-albums");
+                break;
+
+            case "10":
+                case "25":
+                    case "50":
+                        case "100":
+            xmlPanel.setLimit(Integer.parseInt(e.getActionCommand()));
+            break;
+
+            case "Yes":
+                case "No":
+            xmlPanel.setExplicit(Boolean.parseBoolean(e.getActionCommand()));
+            break;
+        }
+    }
+
+    // The main method will create the frame and call the createAndShowGUI to display it
+    public static void main(String[] args) {
+        EventQueue.invokeLater(() -> {
+            xmlDownloader frame = new xmlDownloader("iTunes Store Albums");
+            frame.createAndShowGUI();
+
         });
-
     }
+
 
     //Private members
-
-    private JMenuBar menuBar;
-    private JTextArea showAlbums;
-    private JMenu Type;
-    private JMenu Limit;
-    private JMenu Explicit;
-    private JMenuItem newMusicOption;
-    private JMenuItem recentMusicOption;
-    private JMenuItem topAlbumsOption;
-    private JMenuItem tenOption;
-    private JMenuItem twentyFiveOption;
-    private JMenuItem fiftyOption;
-    private JMenuItem oneHundOption;
-    private JMenuItem yesOption;
-    private JMenuItem noOption;
+    private xmlDownloaderPanel xmlPanel = new xmlDownloaderPanel();
+    private JMenuBar menuBar =  new JMenuBar();
+    private JMenu menu;
+    private JRadioButtonMenuItem menuItem;
+    private ButtonGroup group;
 
 }

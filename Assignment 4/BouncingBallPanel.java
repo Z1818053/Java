36fb267ad8d6bf95a6a489f9e06c79a6701f6b3b
TFranczak <franczaktommy@yahoo.com>
2018-04-16 20:26:46 -0500

    Create BouncingBallPanel.java

---
+++ Assignment 4/BouncingBallPanel.java
@@ -0,0 +1,73 @@
+import javax.swing.*;
+import javax.swing.border.Border;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+/*  BouncingBallPanel Class
+    Combines the animation panel with a button panel interface
+ */
+
+public class BouncingBallPanel extends JPanel implements ActionListener
+{
+    private JButton startButton;
+    private JButton stopButton;
+    private JPanel buttonPanel;
+    private BallAnimationPanel animationPanel;
+
+    /* BouncingBallPanel Constructor
+       This constructor creates the stop and start buttons.
+       It also creates the ball animation panel.
+     */
+    public BouncingBallPanel ()
+    {
+        super();
+        setLayout(new BorderLayout());
+        setPreferredSize(new Dimension(300, 500));
+
+        // Create the "start" button and add an ActionListener
+        startButton = new JButton("Start");
+        startButton.addActionListener(this);
+
+        // Create the "stop" button and add an ActionListener. Disabled initially.
+        stopButton = new JButton("Stop");
+        stopButton.addActionListener(this);
+        stopButton.setEnabled(false);
+
+        // Create the panel and add the buttons
+        buttonPanel = new JPanel();
+        buttonPanel.add(startButton);
+        buttonPanel.add(stopButton);
+
+        // Create the ball animation panel
+        animationPanel = new BallAnimationPanel();
+
+        // Position the panels
+        add(buttonPanel, BorderLayout.PAGE_START);
+        add(animationPanel, BorderLayout.CENTER);
+
+    }
+
+    // actionPerformed
+    // Handles the button clicks and starts and stops the ball animation
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getActionCommand().equals("Start")) {
+            // Begin ball bouncing
+            animationPanel.start();
+
+            // Swap enabled buttons
+            startButton.setEnabled(false);
+            stopButton.setEnabled(true);
+        }
+        else if (e.getActionCommand().equals("Stop"))
+        {
+            // Cease ball bouncing
+            animationPanel.stop();
+
+            // Swap enabled buttons
+            startButton.setEnabled(true);
+            stopButton.setEnabled(false);
+        }
+    }
+}
